875727d38219543c687ae69754afbf83900500f1
Kartik Bagri <kartikbagri199@gmail.com>
2022-04-02 15:36:28 +0530

    adding missing reports to database

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const User = require('./models/userSchema');
+const Report = require('./models/reportSchema');
 const middleware = require('./middleware');
 const multer  = require('multer')
 const upload = multer({ dest: 'face_detection/image_set/' })
 const uploadInput = multer({ dest: 'face_detection/input_image/' })
 const fs = require('fs');
 const path = require('path');
+
+// Facial Recognition
+// const canvas = require('canvas');
+// const faceapi = require('face-api.js');
 
 // Python
 const spawn = require("child_process").spawn;
@@ -74,25 +79,34 @@ app.get('/missing', (req, res) => {
     res.render('missingReport', payload);
 });
 
-app.post('/missing', upload.array('recentImages'), (req, res) => {
-    for(let i = 0; i < req.files.length; i++) {
-        const filePath = req.files[i].originalname;
-        const tempPath = req.files[i].path;
-        try {
-            if (!fs.existsSync('./face_detection/image_set/vikas')) {
-                fs.mkdirSync('./face_detection/image_set/vikas');
-            }
-        } catch(err) {
-            console.log(err);
-            res.sendStatus(400);
-        }
-        const targetPath = path.join(__dirname, `./face_detection/image_set/vikas/${filePath}`);
-        fs.rename(tempPath, targetPath, function(err) {
-            if(err){
+app.post('/missing', upload.array('recentImages'), async (req, res) => {
+    const data = { missingPersonName, gender, age, aadhaarNumber, reporterName, reporterPhoneNumber, reporterAadhaarNumber, reporterAddress } = req.body;
+    const newReport = new Report(data);
+    try {
+        const response = await newReport.save();
+        console.log(response);
+        for(let i = 0; i < req.files.length; i++) {
+            const filePath = req.files[i].originalname;
+            const tempPath = req.files[i].path;
+            try {
+                if (!fs.existsSync(`./face_detection/image_set/${response._id.toString()}`)) {
+                    fs.mkdirSync(`./face_detection/image_set/${response._id.toString()}`);
+                }
+            } catch(err) {
                 console.log(err);
-                return res.sendStatus(400);
+                res.sendStatus(400);
             }
-        });
+            const targetPath = path.join(__dirname, `./face_detection/image_set/${response._id.toString()}/${filePath}`);
+            fs.rename(tempPath, targetPath, function(err) {
+                if(err){
+                    console.log(err);
+                    return res.sendStatus(400);
+                }
+            });
+        }
+    } catch(err) {
+        console.log(err);
+        res.sendStatus(400);
     }
     res.redirect('/');
 })
@@ -102,7 +116,7 @@ app.get('/facescan', (req, res) => {
     res.render('faceScan', payload);
 })
 
-app.post('/facescan', uploadInput.single('inputImg') , (req, res) => {
+app.post('/facescan', uploadInput.single('inputImg') , async (req, res) => {
     const filePath = req.file.filename;
     const tempPath = req.file.path;
     const targetPath = path.join(__dirname, `./face_detection/input_image/${filePath}.png`);
@@ -117,6 +131,21 @@ app.post('/facescan', uploadInput.single('inputImg') , (req, res) => {
         const result = JSON.parse(data);
         res.send(result);
     });
+    // await faceapi.nets.ssdMobilenetv1.loadFromDisk('./weights')
+    // await faceapi.nets.faceLandmark68Net.loadFromDisk('./weights')
+    // await faceapi.nets.faceRecognitionNet.loadFromDisk('./weights')
+    // const { Canvas, Image, ImageData } = canvas
+    // faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+    
+    // const queryImg = await canvas.loadImage('face_detection/input_image/' + filePath + '.png');
+    // const referenceImg = await canvas.loadImage('face_detection/image_set/person/photo.jpeg');
+    // const referenceResults = await faceapi.detectSingleFace(referenceImg).withFaceLandmarks().withFaceDescriptor();
+    // const faceMatcher = new faceapi.FaceMatcher(referenceResults)
+    // const queryResults = await faceapi.detectSingleFace(queryImg).withFaceLandmarks().withFaceDescriptor();
+    // const bestMatch = faceMatcher.findBestMatch(queryResults.descriptor);
+    // console.log(bestMatch);
+    // res.send(bestMatch);
+    // console.log(detection);
 })
 
 app.listen(3000, () => {
